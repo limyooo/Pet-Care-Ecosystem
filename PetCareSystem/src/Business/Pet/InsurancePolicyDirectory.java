376244845eaffb2399066c1f99b107b385c00f8b
Jing-Yang-Wang <yangwang.j@northeastern.edu>
2025-11-25 13:39:21 -0500

    update insurance policy and directory

--- PetCareSystem/src/Business/Pet/InsurancePolicyDirectory.java
+++ PetCareSystem/src/Business/Pet/InsurancePolicyDirectory.java
@@ -4,10 +4,54 @@
  */
 package Business.Pet;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jingyangwang
  */
 public class InsurancePolicyDirectory {
     
+    private ArrayList<InsurancePolicy> policyList = new ArrayList<>();
+    
+    //Constructor
+    public InsurancePolicyDirectory(){
+        
+    }
+    //get Policy list method
+    public ArrayList<InsurancePolicy> getPolicyList() {
+        return policyList;
+    }
+    
+    //add new policy method to the list
+    public InsurancePolicy addPolicy(String policyId, String provider, String coverageType,
+                                     String startDate, String endDate, double premium, 
+                                     Pet pet) {
+
+        InsurancePolicy policy = new InsurancePolicy(policyId, provider, coverageType,
+                                                     startDate, endDate, premium, pet);
+
+        policyList.add(policy);
+        return policy;
+    }
+    //add existing policy from list
+    public void addPolicy(InsurancePolicy policy) {
+        policyList.add(policy);
+    }
+
+    //remove existing policy from list
+    public void removePolicy(InsurancePolicy policy) {
+        policyList.remove(policy);
+    }
+
+    //search by policyId
+    public InsurancePolicy findPolicyById(String policyId) {
+        for (InsurancePolicy policy : policyList) {
+            if (policy.getPolicyId().equals(policyId)) {
+                return policy;
+            }
+        }
+        return null;
+    }
 }
+

8c8c09fa5a9c31501bd890891ea6a4ae456fbe93
LINYAO HAN <han.linya@northeastern.edu>
2025-12-01 18:02:55 -0500

    完成面板
    完成面板

--- PetCareSystem/src/Business/Role/FrontDeskAgentRole.java
+++ PetCareSystem/src/Business/Role/FrontDeskAgentRole.java
@@ -3,10 +3,13 @@ package Business.Role;
 import Business.Petsystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
+import static Business.Organization.Organization.Type.CustomerService;
 import Business.PetClinicOrganization.FrontDeskOrganization;
 import Business.UserAccount.UserAccount;
 import UI.petClinic.FrontDeskManagementJPanel;
 import javax.swing.JPanel;
+import Business.PetBoardingOrganization.CustomerService;
+import UI.Boarding.CustomerServiceJPanel;
 
 /**
  * FrontDeskAgentRole:
@@ -15,8 +18,18 @@ import javax.swing.JPanel;
 public class FrontDeskAgentRole extends Role {
 
     @Override
-    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Petsystem system) {
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account,
+                                 Organization organization, Enterprise enterprise, Petsystem system) {
         
-        return new FrontDeskManagementJPanel(userProcessContainer,account,(FrontDeskOrganization) organization,enterprise);
+        // ⭐ 判断是 Boarding 的 CustomerService 还是 Clinic 的 FrontDesk
+        if (organization instanceof CustomerService) {
+            // Pet Boarding 的客服 → 跳转到 CustomerServiceJPanel
+            return new CustomerServiceJPanel(userProcessContainer, account, 
+                    (CustomerService) organization, enterprise, system);
+        } else {
+            // Pet Clinic 的前台 → 跳转到 FrontDeskManagementJPanel
+            return new FrontDeskManagementJPanel(userProcessContainer, account,
+                    (FrontDeskOrganization) organization, enterprise);
+        }
     }
 }
